5b3feac4996a8b1c807cfe7ac4fbb06fd52886bf
SurinderSingh12 <sabysurinder@gmail.com>
2023-07-01 12:36:00 -0700

    Changed Configration for deployment
    Changed the start command and app listening port

--- back-end/server.js
+++ back-end/server.js
@@ -42,7 +42,7 @@ app.use((error, req, res, next) => {
 mongoose
 .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.orpjczc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 .then(() => {
-  app.listen(3001);
+  app.listen(process.env.PORT || 3001);
 })
 .catch(err =>{
   console.log(err);
